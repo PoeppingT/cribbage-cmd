b6bd1b14549ee568718f8a73981e92432069b756
PoeppingT <poeppt@amazon.com>
2022-09-13 16:45:19 -0700

    Add basic main menu and start of jvmoptions

--- src/main/java/org/poepping/dev/MainMenu.java
+++ src/main/java/org/poepping/dev/MainMenu.java
@@ -16,37 +16,32 @@ package org.poepping.dev;
 
 import org.poepping.dev.gamelogic.Config;
 
+import java.util.Scanner;
+
 public class MainMenu {
   public static void main(String[] args) {
 
-    /*
-     * Welcome to Cribbage!
-     * 1. Start Game
-     * 2. View Stats
-     * 3. Exit
-     */
+    boolean exited = false;
+    Scanner in = new Scanner(System.in);
 
-    CribbageGame.Builder cgb = CribbageGame.builder();
-    if (args.length > 0) {
-      // assume they've passed the score
-      try {
-        int scoreToWin = Integer.parseInt(args[0]);
-        cgb.config(Config.builder().maxScore(scoreToWin).build());
-        System.out.println("At least one argument passed,"
-            + " starting a Cribbage Game where the score to win is " + scoreToWin + "...");
-      } catch (NumberFormatException nfe) {
-        usage();
-        System.exit(2);
+    while (!exited) {
+      System.out.println("====================================");
+      System.out.println("|         Cribbage Game!           |");
+      System.out.println("====================================");
+      System.out.println("Main Menu:");
+      System.out.println("\t1. Play a game.");
+      System.out.println("\t2. Exit.");
+      String input = in.nextLine();
+      if (input.equals("1")) {
+        // uh..
+        CribbageGame.builder().config(Config.builder().build()).build().run();
+      } else if (input.equals("2")) {
+        exited = true;
+      } else {
+        System.err.println("Input either 1 or 2.");
       }
-    } else {
-      System.out.println("No arguments passed, starting a standard Cribbage Game...");
-      cgb.config(Config.builder().build());
     }
-    System.out.println();
-    cgb.build().run();
-  }
 
-  private static void usage() {
-    System.out.println("CribbageGame <scoreToWin>");
+    in.close();
   }
 }
--- src/main/java/org/poepping/dev/MainOptions.java
+++ src/main/java/org/poepping/dev/MainOptions.java
@@ -5,6 +5,58 @@ package org.poepping.dev;
  *   --ui [text|swing|jetty]
  *   --headless : uses no UI, accepts no user input(?). required to be paired with other options (AI training?)
  */
-public class MainOptions {
-  
+public final class MainOptions {
+
+  private static enum Option {
+    UI("--ui",
+        "To specify a different UI experience.",
+        "text", 
+        new String[]{"text"}, 
+        true);
+
+    final String commandLineOption;
+    final String description;
+    final String defaultValue;
+    final String[] allowedValues;
+    final boolean acceptsValue;
+
+    private Option(
+        String commandLineOption,
+        String description,
+        String defaultValue,
+        String[] allowedValues,
+        boolean acceptsValue) {
+      this.commandLineOption = commandLineOption;
+      this.description = description;
+      this.defaultValue = defaultValue;
+      this.allowedValues = allowedValues;
+      this.acceptsValue = acceptsValue;
+    }
+
+    static Option forCommandLineOption(String commandLineOption) {
+      for (Option option : values()) {
+        if (option.commandLineOption.equals(commandLineOption)) {
+          return option;
+        }
+      }
+      throw new IllegalArgumentException("No such option with commandLineOption " + commandLineOption);
+    }
+  }
+
+  public static MainOptions parseArgs(String[] args) {
+    return new MainOptions();
+  }
+
+  public static String usage() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Usage: \n");
+    for (Option o : Option.values()) {
+      sb.append("\t");
+      sb.append(o.commandLineOption);
+      if (o.acceptsValue) {
+        // do something
+      }
+    }
+    return sb.toString();
+  }
 }
